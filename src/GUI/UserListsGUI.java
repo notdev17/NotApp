1b34be0d39e8b1eb8a7188c83096f573a35f9ccb
Brandon G <Bgaetaniello@gmail.com>
2017-11-30 16:22:30 -0800

    update
    userListsGUI

---
+++ src/GUI/UserListsGUI.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
+// import User;
+// import Appliance
+
+public class UserListsGUI
+{
+    public UserListsGUI()
+    {
+
+    }
+
+    public List<Appliance> getFavorites()
+    {
+
+    }
+
+    public List<Appliance> getExport()
+    {
+
+    }
+
+    public void setUp()
+    {
+
+    }
+}
