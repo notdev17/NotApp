b9ad4b6c186c571206beeab2814e6d7a79644c60
Brandon G <Bgaetaniello@gmail.com>
2017-11-30 16:12:07 -0800

    front page gui class

---
+++ src/GUI/FrontPageGUI.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FrontPageGUI
+{
+    public FrontPageGUI()
+    {
+
+    }
+
+    public void setUp()
+    {
+
+    }
+
+    public void begin()
+    {
+
+    }
+
+}
