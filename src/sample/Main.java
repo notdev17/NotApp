dcefc0686b3424d84d78ab4cda88f5c099814bbe
michellesuchang <michellesuchang@gmail.com>
2017-12-05 19:13:32 -0800

    new

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("FirstPage.fxml"));
+        primaryStage.setTitle("Not App: Compare Appliances!");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
