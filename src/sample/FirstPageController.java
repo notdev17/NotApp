4914d4342a24a9be396e21afaf3a7b7199538d20
michellesuchang <michellesuchang@gmail.com>
2017-12-05 19:52:57 -0800

    transition from pages

--- src/sample/FirstPageController.java
+++ src/sample/FirstPageController.java
@@ -1,9 +1,32 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FirstPageController {
 
     @FXML
-    public void
+    public void getStartedButtonClicked(ActionEvent event) throws IOException{
+        Parent compareAppParent = FXMLLoader.load(getClass().getResource("CompareAppliances.fxml"));
+        Scene compareAppScene = new Scene(compareAppParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(compareAppScene);
+
+        window.show();
+
+    }
+
+    @FXML
+    public void tutorialButtonClicked(ActionEvent event){
+
+
+    }
 }
