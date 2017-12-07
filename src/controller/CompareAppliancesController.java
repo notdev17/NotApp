8dce165cf7cbc9a5f04435670b27e3277a403aa0
flannyan <daylennguyen@gmail.com>
2017-12-07 14:25:50 -0800

    Group commit desu

--- src/controller/CompareAppliancesController.java
+++ src/controller/CompareAppliancesController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CompareAppliancesController {
+
+    @FXML
+    public void createApplianceButtonClicked(ActionEvent event) throws IOException {
+//        Parent compareAppParent = FXMLLoader.load(getClass().getResource("../fxml/CompareAppliances.fxml"));
+//        Scene compareAppScene = new Scene(compareAppParent);
+//
+//        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        window.setScene(compareAppScene);
+//
+//        window.show();
+        System.out.println("This button works!");
+    }
 }
