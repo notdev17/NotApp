1fc618f7cd5def87872da880868209aae0f214ed
Brandon G <Bgaetaniello@gmail.com>
2017-12-12 03:11:47 -0800

    Revert "Revert "updated compareAppliances page, added future appliance information fxml.""
    This reverts commit 2230fffb54c49d64eaa4da4e904152ab6fe6a6a9.

--- src/controller/CompareAppliancesController.java
+++ src/controller/CompareAppliancesController.java
@@ -1,11 +1,18 @@
 package controller;
 
+import appliance.Appliance;
+import appliance.AirConditioner;
+import appliance.Refrigerator;
+import appliance.WashingMachine;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
