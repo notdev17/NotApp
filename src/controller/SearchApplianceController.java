e67b7aebb677d43b3738048b64e02da33761cfdb
Flan <daylennguyen@gmail.com>
2017-12-14 06:52:19 -0800

    Revert "small change to populate the table in search appliance"
    This reverts commit bc6b600d0fb9868a360def2654698150b8003b7c.

--- src/controller/SearchApplianceController.java
+++ src/controller/SearchApplianceController.java
@@ -82,7 +82,7 @@ public class SearchApplianceController
                   When a user clicks an appliance type in the drop down menu, it will be re-populated with the proper
                   appliances (using getAppliances).
                  */
-                myTableView.setItems(getAppliances(myApplianceBox.getItems().get(newValue.intValue())));
+                getAppliances(myApplianceBox.getItems().get(newValue.intValue()));
             }
         });
     }
