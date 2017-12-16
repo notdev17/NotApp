828fe095c8c5b06ccaf864e92a10afda7e433148
Tim <timonyang@gmail.com>
2017-12-16 05:42:52 -0800

    minor updates

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -4,6 +4,8 @@ import controller.MasterController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
 public class Main extends Application {
 
     private MasterController mc;
