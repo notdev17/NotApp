628db6c430e7b5e589625a712fe0fcadc52fbc64
Tim <timonyang@gmail.com>
2017-12-16 06:01:43 -0800

    executable jar take 3

--- src/controller/MasterController.java
+++ src/controller/MasterController.java
@@ -37,7 +37,7 @@ public class MasterController {
     }
 
     void getFirstPage() throws IOException {
-        myParent = myLoader.load(getClass().getResource("../fxml/FirstPage.fxml").openStream());
+        myParent = myLoader.load(getClass().getClassLoader().getResource("fxml/FirstPage.fxml").openStream());
         ((FirstPageController) myLoader.getController()).setMasterController(this);
         myStage.setScene(new Scene(myParent));
         myStage.show();
@@ -45,7 +45,7 @@ public class MasterController {
     }
 
     void getComparePage() throws IOException {
-        myParent = myLoader.load(getClass().getResource("../fxml/CompareAppliances.fxml").openStream());
+        myParent = myLoader.load(getClass().getClassLoader().getResource("fxml/CompareAppliances.fxml").openStream());
         ((CompareAppliancesController) myLoader.getController()).setMasterController(this);
         myStage.setScene(new Scene(myParent));
         myStage.show();
@@ -53,7 +53,7 @@ public class MasterController {
     }
 
     void getSearchPage() throws IOException {
-        myParent = myLoader.load(getClass().getResource("../fxml/SearchAppliance.fxml").openStream());
+        myParent = myLoader.load(getClass().getClassLoader().getResource("fxml/SearchAppliance.fxml").openStream());
         ((SearchApplianceController) myLoader.getController()).setMasterController(this);
         myStage.setScene(new Scene(myParent));
         myStage.show();
@@ -61,7 +61,7 @@ public class MasterController {
     }
 
     void getCalculationsPage() throws IOException {
-        myParent = myLoader.load(getClass().getResource("../fxml/CalculateSavings.fxml").openStream());
+        myParent = myLoader.load(getClass().getClassLoader().getResource("fxml/CalculateSavings.fxml").openStream());
         ((CalculateSavingsController) myLoader.getController()).setMasterController(this);
         myStage.setScene(new Scene(myParent));
         myStage.show();
@@ -69,7 +69,7 @@ public class MasterController {
     }
 
     void getUserPage() throws IOException {
-        myParent = myLoader.load(getClass().getResource("../fxml/User.fxml").openStream());
+        myParent = myLoader.load(getClass().getClassLoader().getResource("fxml/User.fxml").openStream());
         ((UserController) myLoader.getController()).setMasterController(this);
         myStage.setScene(new Scene(myParent));
         myStage.show();
