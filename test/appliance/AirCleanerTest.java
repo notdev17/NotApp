9afd5a9a71bcad1c8d5285aed4695e7d4c8e1ff0
Daylen Nguyen <daylennguyen@gmail.com>
2018-08-24 23:39:30 -0700

    Preparing for documentation

--- test/appliance/AirCleanerTest.java
+++ test/appliance/AirCleanerTest.java
@@ -3,33 +3,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Created by notDev on 12/15/2017
- */
 class AirCleanerTest {
 
     private AirCleaner airCleaner= new AirCleaner();
 
-    /**
-     * Author: Brandon on 12/15/2017
-     */
     @Test
     void getCopy() {
         AirCleaner airCleaner2 = new AirCleaner();
         assertEquals(airCleaner2.toString(), airCleaner.toString());
     }
 
-    /**
-     * Author: Daylen on 12/15/2017
-     */
     @Test
     void getApplianceType() {
         assertEquals(airCleaner.getApplianceType(), "Air Cleaner");
     }
 
-    /**
-     * Author: Michelle on 12/15/2017
-     */
     @Test
     void toStringTest() {
         airCleaner = new AirCleaner("Model", "Brand", 200);
